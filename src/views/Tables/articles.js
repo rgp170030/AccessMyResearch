0c70a4bbfa6b25ec3d45cd1e02edd229453a6a28
Caleb Hoff <crunchycat1@gmail.com>
2020-10-07 18:24:28 -0500

    Added Example Schema for User, Project, & Article

--- src/views/Tables/articles.js
+++ src/views/Tables/articles.js
@@ -1,7 +1,8 @@
 export default [
   {
-    img: 'img/theme/bootstrap.jpg',
+    id: 1,
     title: 'Continuous demixing transition of binary liquids: finite-size scaling from the analysis of sub-systems',
+    img: 'img/theme/bootstrap.jpg',
     author: 'Caleb Hoff',
     date: '30 September 2020',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -9,8 +10,9 @@ export default [
     rating: 60
   },
   {
-    img: 'img/theme/angular.jpg',
+    id: 2,
     title: 'Precise phase control of large-scale inorganic perovskites via vapor-phase anion-exchange strategy',
+    img: 'img/theme/angular.jpg',
     author: 'Caleb Hoff',
     date: '30 September 2020',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -18,8 +20,9 @@ export default [
     rating: 100
   },
   {
-    img: 'img/theme/sketch.jpg',
+    id: 3,
     title: 'Investigating institutional influence on graduate program admissions by modelling physics GRE cut-off scores',
+    img: 'img/theme/sketch.jpg',
     author: 'Mehmet Gunal',
     date: '30 September 2020',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -27,8 +30,9 @@ export default [
     rating: 72
   },
   {
-    img: 'img/theme/react.jpg',
+    id: 4,
     title: 'John von Neumann\'s 1950s Change to Philosopher of Computation',
+    img: 'img/theme/react.jpg',
     author: 'Spencer Balog',
     date: '30 September 2020',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -36,8 +40,9 @@ export default [
     rating: 90
   },
   {
-    img: 'img/theme/vue.jpg',
+    id: 5,
     title: 'An information theoretic network approach to socioeconomic correlations',
+    img: 'img/theme/vue.jpg',
     author: 'Caleb Hoff',
     date: '25 December 1986',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -45,8 +50,9 @@ export default [
     rating: 100
   },
   {
-    img: 'img/theme/bootstrap.jpg',
+    id: 6,
     title: 'A posteriori corrections to the Iterative Qubit Coupled Cluster method to minimize the use of quantum resources in large-scale calculations',
+    img: 'img/theme/bootstrap.jpg',
     author: 'Greg Kitchen',
     date: '26 February 2017',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -54,8 +60,9 @@ export default [
     rating: 60
   },
   {
-    img: 'img/theme/angular.jpg',
+    id: 7,
     title: 'Towards Universal Sparse Gaussian Process Potentials: Application to Lithium Diffusivity in Superionic Conducting Solid Electrolytes',
+    img: 'img/theme/angular.jpg',
     author: 'Mehmet Gunal',
     date: '31 December 2019',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -63,8 +70,9 @@ export default [
     rating: 100
   },
   {
-    img: 'img/theme/sketch.jpg',
+    id: 8,
     title: 'Universal coherent structures of elastic turbulence in straight channel with viscoelastic fluid flow',
+    img: 'img/theme/sketch.jpg',
     author: 'Daksh Kapur',
     date: '1 January 2018',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -72,8 +80,9 @@ export default [
     rating: 72
   },
   {
-    img: 'img/theme/vue.jpg',
+    id: 9,
     title: 'Novel Technosignatures',
+    img: 'img/theme/vue.jpg',
     author: 'Caleb Hoff',
     date: '23 May 2011',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
@@ -81,8 +90,9 @@ export default [
     rating: 100
   },
   {
-    img: 'img/theme/vue.jpg',
+    id: 10,
     title: 'Implications of Gravitational-wave Production from Dark Photon Resonance to NANOGrav Observations and Effective number of Relativistic Species',
+    img: 'img/theme/vue.jpg',
     author: 'Caleb Hoff',
     date: '19 April 2019',
     abstract: 'Anion exchange offers great flexibility and high precision in phase control, compositional engineering and optoelectronic property tuning. Different from previous successful anion exchange process in liquid solution, herein, we develop a vapor-phase anion-exchange strategy to realize the precise phase and bandgap control of large-scale inorganic perovskites by using gas injection cycle, produing some perovskites such as CsPbCl3 which has never been reported in thin film morphology. Ab-initio calculations also provide the insightful mechanism to understand the impact of anion exchange on tuning the electronic properties and optimizing the structural stability. Furthermore, because of precise control of specific atomic concentrations, intriguing tunable photoluminsecence is observed and photodetectors with tunable photoresponse edge from green to ultraviolet light can be realized accurately with an ultrahigh spectral resolution of 1 nm. Therefore, we offer a new, universal vapor-phase anion exchange method for inorganic perovskite with fine-tunable optoelectronic properties.',
---
+++ src/views/Tables/projects.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    id: 1,
+    name: 'My AI Project',
+    img: 'img/theme/team-1.jpg',
+    createdAt: '10/09/2018',
+    owners: [5],
+    members: [1, 3],
+    collections: [2, 5, 1]
+  },
+  {
+    id: 2,
+    name: 'Water is not real',
+    img: 'img/theme/team-2.jpg',
+    createdAt: '08/09/2018',
+    owners: [4],
+    members: [2, 1],
+    collections: [4]
+  },
+  {
+    id: 3,
+    name: 'Mehmet\'s Project',
+    img: 'img/theme/team-3.jpg',
+    createdAt: '30/08/2018',
+    owners: [4, 5],
+    members: [1],
+    collections: [3, 1]
+  },
+  {
+    id: 4,
+    name: 'Electricity Project',
+    img: 'img/theme/team-1.jpg',
+    createdAt: '10/09/2018',
+    owners: [1],
+    members: [2],
+    collections: [2, 1, 5]
+  },
+  {
+    id: 5,
+    name: 'GMO Research',
+    img: 'img/theme/team-2.jpg',
+    createdAt: '30/09/2018',
+    owners: [3],
+    members: [4, 1],
+    collections: [1, 2, 4]
+  },
+];
