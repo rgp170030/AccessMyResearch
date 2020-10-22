874027ff72ed273913294b73f0b8fcc895524894
Modeste17 <axa170011@utdallas.edu>
2020-10-22 00:44:22 -0500

    I add admin page and import the different  file into the routes.js

---
+++ src/views/Admins/Admin.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="alert alert-info">
+      <strong>Administrator</strong>
+    </div>
+    <h2>Login</h2>
+    <form>
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">never share your email with anyone else.</small>
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <small id="passworHelp" class="form-text text-muted">never share your password with anyone else.</small>
+      </div>
+      <button type="submit" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+
+</template>
+
+
