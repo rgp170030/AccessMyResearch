1842ee4ef263f6d36416251cff8887d6c25f0d6e
SIB170001 <SIB170001@utdallas.edu>
2020-11-07 14:35:09 -0600

    Beginning work on user roles using cognito groups

---
+++ src/util/userroles.js
@@ -0,0 +1,29 @@
+
+
+export default {
+    isAdmin: function (user) {
+        // the array of groups that the user belongs to
+        const groups = user.signInUserSession.accessToken.payload["cognito:groups"];
+        for (var i = 0; i < groups.length; i++) {
+            switch (groups[i]) {
+                case "AMR_Admin":
+                    return true;
+            }
+        }
+    },
+    getUserRoles: function (user) {
+        // the array of groups that the user belongs to
+        var roles = [];
+
+        const groups = user.signInUserSession.accessToken.payload["cognito:groups"];
+        for (var i = 0; i < groups.length; i++) {
+            switch (groups[i]) {
+                case "AMR_Admin":
+                    roles.push("Administrator");
+                    break;
+            }
+        }
+
+        return roles;
+    }
+}
