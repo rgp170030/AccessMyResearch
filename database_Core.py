18537e2a8f30195761f30ac03bd99c66ca73d4d6
Syed AbuTalib <syedthegreatabutalib@gmail.com>
2020-11-10 15:53:32 -0600

    made database_Core into a method to be called by indexer.py

--- database_Core.py
+++ database_Core.py
@@ -3,106 +3,107 @@ import urllib.parse
 import json
 from elasticsearch import Elasticsearch
 
-with open('search.json') as f:
-    search_terms = json.load(f)['search']
-    for i in range(len(search_terms)):
-        search_terms[i] = '"' + search_terms[i].replace(' ', ' AND ') + '"'
+def core(keywords):
+    with open(keywords) as f:
+        search_terms = json.load(f)['search']
+        for i in range(len(search_terms)):
+            search_terms[i] = '"' + search_terms[i].replace(' ', ' AND ') + '"'
 
-class CoreApiRequestor:
-    def __init__(self, endpoint, api_key):
-        self.endpoint = endpoint
-        self.api_key = api_key
-        # defaults
-        self.pagesize = 100
-        self.page = 1
+    class CoreApiRequestor:
+        def __init__(self, endpoint, api_key):
+            self.endpoint = endpoint
+            self.api_key = api_key
+            # defaults
+            self.pagesize = 100
+            self.page = 1
 
-    def parse_response(self, decoded):
-        res = []
-        for item in decoded['data']:
-            doi = None
-            if 'identifiers' in item:
-                for identifier in item['identifiers']:
-                    if identifier and identifier.startswith('doi:'):
-                        doi = identifier
-                        break
-            res.append([item['title'], doi])
-        return res
+        def parse_response(self, decoded):
+            res = []
+            for item in decoded['data']:
+                doi = None
+                if 'identifiers' in item:
+                    for identifier in item['identifiers']:
+                        if identifier and identifier.startswith('doi:'):
+                            doi = identifier
+                            break
+                res.append([item['title'], doi])
+            return res
 
-    def request_url(self, url):
-        with urllib.request.urlopen(url) as response:
-            html = response.read()
-        return html
+        def request_url(self, url):
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+            return html
 
-    def get_method_query_request_url(self, method, query, fullText, page):
-        if (fullText):
-            fullText = 'true'
-        else:
-            fullText = 'false'
-        params = {
-            'apiKey': self.api_key,
-            'page': page,
-            'pageSize': self.pagesize,
-            'fulltext': fullText,
-            'urls': 'true'
-        }
-        return self.endpoint + method + '/' + urllib.parse.quote(query) + '?' + urllib.parse.urlencode(params)
+        def get_method_query_request_url(self, method, query, fullText, page):
+            if (fullText):
+                fullText = 'true'
+            else:
+                fullText = 'false'
+            params = {
+                'apiKey': self.api_key,
+                'page': page,
+                'pageSize': self.pagesize,
+                'fulltext': fullText,
+                'urls': 'true'
+            }
+            return self.endpoint + method + '/' + urllib.parse.quote(query) + '?' + urllib.parse.urlencode(params)
 
-    def get_up_to_x_pages_of_query(self, method, query, fulltext, x = 20):
-        url = self.get_method_query_request_url(method, query, fulltext, 1)
-        all_articles = []
-        resp = self.request_url(url)
-        result = json.loads(resp.decode('utf-8'))
-        all_articles.append(result)
-        if (result['totalHits'] > 100):
-            numOfPages = int(result['totalHits']/self.pagesize)  # rounds down
-            if (numOfPages > x + 1):
-                numOfPages = x + 1
-            for i in range(2, numOfPages):
-                url = self.get_method_query_request_url(
-                    method, query, False, i)
-                print(url)
-                resp = self.request_url(url)
-                all_articles.append(json.loads(resp.decode('utf-8')))
-        return all_articles
-
-
-endpoint = 'https://core.ac.uk/api-v2'
-api_key = '5TphODMbCjoQUkWitNZSBVqsEeYlHfnI'
-method = '/articles/search'
-topic = '*'
-
-api = CoreApiRequestor(endpoint, api_key)
-result = []
-for term in search_terms:
-    result += api.get_up_to_x_pages_of_query(method, term, False, x = 2)
+        def get_up_to_x_pages_of_query(self, method, query, fulltext, x = 20):
+            url = self.get_method_query_request_url(method, query, fulltext, 1)
+            all_articles = []
+            resp = self.request_url(url)
+            result = json.loads(resp.decode('utf-8'))
+            all_articles.append(result)
+            if (result['totalHits'] > 100):
+                numOfPages = int(result['totalHits']/self.pagesize)  # rounds down
+                if (numOfPages > x + 1):
+                    numOfPages = x + 1
+                for i in range(2, numOfPages):
+                    url = self.get_method_query_request_url(
+                        method, query, False, i)
+                    print(url)
+                    resp = self.request_url(url)
+                    all_articles.append(json.loads(resp.decode('utf-8')))
+            return all_articles
 
 
-def clean(result):
-    mega_json = []
-    for p in result:
-        for a in p['data']:
-            try:
-                obj = {}
-                k = ['authors', 'title', 'description',
-                     'datePublished', 'fulltextUrls', 'journal', 'doi']
-                for i in k:
-                    if i in a:
-                        obj[i] = a[i]
-                if 'fulltextUrls' in obj:
-                    obj['url'] = obj.pop('fulltextUrls')
-                mega_json.append(obj)
-            except:
-                print('Error occured while cleaning')
-    return mega_json
+    endpoint = 'https://core.ac.uk/api-v2'
+    api_key = '5TphODMbCjoQUkWitNZSBVqsEeYlHfnI'
+    method = '/articles/search'
+    topic = '*'
+
+    api = CoreApiRequestor(endpoint, api_key)
+    result = []
+    for term in search_terms:
+        result += api.get_up_to_x_pages_of_query(method, term, False, x = 2)
 
 
-cleaned_data = clean(result)
+    def clean(result):
+        mega_json = []
+        for p in result:
+            for a in p['data']:
+                try:
+                    obj = {}
+                    k = ['authors', 'title', 'description',
+                        'datePublished', 'fulltextUrls', 'journal', 'doi']
+                    for i in k:
+                        if i in a:
+                            obj[i] = a[i]
+                    if 'fulltextUrls' in obj:
+                        obj['url'] = obj.pop('fulltextUrls')
+                    mega_json.append(obj)
+                except:
+                    print('Error occured while cleaning')
+        return mega_json
 
-es = Elasticsearch()
 
-for doc in range(len(cleaned_data)):
-    res = es.index(index="core", id=doc, body=cleaned_data[doc])
-    if doc%100 == 0:
-        print(doc)
+    cleaned_data = clean(result)
 
-es.indices.refresh(index="core")
+    es = Elasticsearch()
+
+    for doc in range(len(cleaned_data)):
+        res = es.index(index="core", id=doc, body=cleaned_data[doc])
+        if doc%100 == 0:
+            print(doc)
+
+    es.indices.refresh(index="core")
