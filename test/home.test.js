b57c683f0737150fe516beceba664fea8ed336d3
Caleb Hoff <crunchycat1@gmail.com>
2020-11-11 04:32:56 -0600

    Changes to testing behavior

--- test/home.test.js
+++ test/home.test.js
@@ -1,19 +1,32 @@
 import { shallowMount } from "@vue/test-utils";
-import Home from "@/views/Home";
+// import Home from "@/views/Home";
 
-function createConfig (overrides) {
-    const id = 1;
-    const mocks = {
-      // Vuex
-      $store: {
-        state: [ { id } ],
-        commit: () => {}
-      }
-    };
-    const propsData = { id };
-    return Object.assign({ mocks, propsData }, overrides);
-  }
+// function createConfig (overrides) {
+//     const id = 1;
+//     const mocks = {
+//       // Vuex
+//       $store: {
+//         state: [ { id } ],
+//         commit: () => {}
+//       }
+//     };
+//     const propsData = { id };
+//     return Object.assign({ mocks, propsData }, overrides);
+//   }
   
-  describe("Item.vue", () => {
-    // Tests go here
-  });
+//   describe("Item.vue", () => {
+//     // Tests go here
+//   });
+
+//Example test to check math
+describe('Checking Math', () => {
+
+    test('Simple Addition', () => {
+        expect(1 + 4).toBe(5);
+    });
+
+    test('Simple Subtraction', () => {
+        expect(6 - 2).toBe(4);
+    });
+
+});
