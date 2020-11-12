911fbb61fc4fba1d5d1b45fd9eb1a08e51f8d08f
Anthony-Tien N Huynh <34702227+Ant1997@users.noreply.github.com>
2020-11-11 20:00:06 -0600

    Error DOAJ Python Scraper

---
+++ database_DOAJ.py
@@ -0,0 +1,86 @@
+import urllib.request
+import urllib.parse
+import json
+from elasticsearch import Elasticsearch
+
+def core(keywords):
+    with open(keywords) as f:
+        search_terms = json.load(f)['search']
+        for i in range(len(search_terms)):
+            search_terms[i] = '"' + search_terms[i].replace(' ', ' AND ') + '"'
+
+    class DOAJApiRequestor:
+        def __init__(self, endpoint):
+            self.endpoint = endpoint
+            # defaults
+            self.pagesize = 100
+            self.page = 1
+
+        def request_url(self, url):
+            with urllib.request.urlopen(url) as response:
+                html = response.read()
+            return html
+
+        def get_method_query_request_url(self, method, query, page):
+            params = {
+                'page': page,
+                'pageSize': self.pagesize,
+            }
+            return self.endpoint + method + '/' + urllib.parse.quote(query) + '?' + urllib.parse.urlencode(params)
+
+        def get_up_to_x_pages_of_query(self, method, query, x = 20):
+            url = self.get_method_query_request_url(method, query, 1)
+            all_articles = []
+            resp = self.request_url(url)
+            result = json.loads(resp.decode('utf-8'))
+            all_articles.append(result)
+            if (result['total'] > 100):
+                numOfPages = int(result['total']/self.pagesize)  # rounds down
+                if (numOfPages > x + 1):
+                    numOfPages = x + 1
+                for i in range(2, numOfPages):
+                    url = self.get_method_query_request_url(
+                        method, query, i)
+                    print(url)
+                    resp = self.request_url(url)
+                    all_articles.append(json.loads(resp.decode('utf-8')))
+            return all_articles
+
+
+    endpoint = 'https://doaj.org/api/v1/'
+    method = 'search/articles'
+    #topic = '*'
+
+    api = DOAJApiRequestor(endpoint)
+    result = []
+    for term in search_terms:
+        result += api.get_up_to_x_pages_of_query(method, term, x = 2)
+
+
+    def clean(result): #need to edit this below
+        mega_json = []
+        for p in result:
+            for aa in p['results']: #.created_date is outside bibjson
+                for a in aa['bibjson']:
+                    try:
+                        obj = {}
+                        k = ['author', 'title', 'abstract', 'link']
+                        for i in k:
+                            if i in a:
+                                obj[i] = a[i]
+                        mega_json.append(obj)
+                    except:
+                        print('Error occured while cleaning')
+        return mega_json
+
+#need to edit this above
+    cleaned_data = clean(result)
+
+    es = Elasticsearch()
+
+    for doc in range(len(cleaned_data)):
+        res = es.index(index="doaj", id=doc, body=cleaned_data[doc])
+        if doc%100 == 0:
+            print(doc)
+
+    es.indices.refresh(index="doaj")
--- indexer.py
+++ indexer.py
@@ -1,5 +1,7 @@
 import multiprocessing
-from database_Core import core
+from database_DOAJ import core
+#from database_DOAJ import doaj
+#from database_Unpaywall import core
 
 def pp():
     for i in range(10):
@@ -9,10 +11,14 @@ def pp():
 keywords = 'search.json'
 if __name__ == '__main__':
     p = multiprocessing.Process(target=core, args=(keywords,))
+    #pdoaj = multiprocessing.Process(target=doaj, args=(keywords,))
+    #p = multiprocessing.Process(target=core, args=(keywords,))
     p2 = multiprocessing.Process(target=pp)
 
     p.start()
+    #pdoaj.start()
     p2.start()
 
     p.join()
+    #pdoaj.join()
     p2.join()
