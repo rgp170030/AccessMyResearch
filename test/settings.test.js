7baa9526092b8c1089e88c071b27b5d718d8d649
Caleb Hoff <crunchycat1@gmail.com>
2020-11-12 19:27:40 -0600

    Testing update

---
+++ test/settings.test.js
@@ -0,0 +1,15 @@
+//This file is used to test the settings tabs
+import { shallowMount } from '@vue/test-utils';
+import ManageIgnoredUsersTab from '@/components/Settings/ManageIgnoredUsersTab.vue'
+
+describe('Testing Ignored Users Tab', () => {
+    const wrapper = shallowMount(ManageIgnoredUsersTab);
+
+    it('Eye icon switches on hover', () => {
+
+        expect(wrapper.vm.getUnignoreIcon(false)).toBe('eye-slash'); //initial title Hello
+        expect(wrapper.vm.getUnignoreIcon(true)).toBe('eye'); // title updates to Hi
+
+    })
+
+})
