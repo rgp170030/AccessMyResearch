cd9e75e1b993022ecd3739fae52961980bc9405e
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2021-02-23 22:28:53 -0600

    Switched from multiprocessing to multithreading to reduce overhead and improved the readability of parts of indexer and CORE

--- python-scraper/database_Core.py
+++ python-scraper/database_Core.py
@@ -2,13 +2,14 @@ import urllib.request
 import urllib.parse
 import json
 import uuid
-from elasticsearch import Elasticsearch
+# from elasticsearch import Elasticsearch
 
 def core(keywords):
     with open(keywords) as f:
         search_terms = json.load(f)['search']
         for i in range(len(search_terms)):
             search_terms[i] = '"' + search_terms[i].replace(' ', ' AND ') + '"'
+    print("Search terms: ", search_terms)
 
     class CoreApiRequestor:
         def __init__(self, endpoint, api_key):
@@ -19,9 +20,14 @@ def core(keywords):
             self.page = 1
 
         def request_url(self, url):
-            with urllib.request.urlopen(url) as response:
-                html = response.read()
-            return html
+            try: 
+                with urllib.request.urlopen(url) as response:
+                    html = response.read()
+                return html
+            except Exception as error:
+                print("Error while requesting: ", url)
+                print(error)
+                return None
 
         def get_method_query_request_url(self, method, query, fullText, page):
             if (fullText):
@@ -39,22 +45,25 @@ def core(keywords):
 
         def get_up_to_x_pages_of_query(self, method, query, fulltext, x = 20):
             url = self.get_method_query_request_url(method, query, fulltext, 1)
-            all_articles = []
-            resp = self.request_url(url)
-            result = json.loads(resp.decode('utf-8'))
-            #print(url)
-            all_articles.append(result)
-            if (result['totalHits'] > 100):
-                numOfPages = int(result['totalHits']/self.pagesize)  # rounds down
-                if (numOfPages > x + 1):
-                    numOfPages = x + 1
-                for i in range(2, numOfPages):
-                    url = self.get_method_query_request_url(
-                        method, query, False, i)
-                    #print(url)
-                    resp = self.request_url(url)
-                    all_articles.append(json.loads(resp.decode('utf-8')))
-            return all_articles
+            page_responses = []
+            response = self.request_url(url)
+            result = json.loads(response.decode('utf-8'))
+
+            page_responses.append(result)
+            if (result['totalHits'] > self.pagesize):
+                numOfPages = int(result['totalHits'] / self.pagesize) + 1 # Integer division
+                numOfPages = min(numOfPages, x)
+                
+                for i in range(2, numOfPages + 1):
+                    url = self.get_method_query_request_url(method, query, False, i)
+                    response = self.request_url(url)
+                    if response is not None:
+                        page_responses.append(json.loads(response.decode('utf-8')))
+                        print("\t - Recieved page #", i)
+                    else: 
+                         print("\t - Server error on page #", i)
+            print("Finished term: ", query)
+            return page_responses
 
 
     endpoint = 'https://core.ac.uk/api-v2'
@@ -66,8 +75,8 @@ def core(keywords):
     result = []
     for term in search_terms:
         result += api.get_up_to_x_pages_of_query(method, term, False, x = 2)
-
-
+    # result += api.get_up_to_x_pages_of_query(method, search_terms[1], False, x = 7)
+    
     def clean(result):
         mega_json = []
         for p in result:
@@ -91,34 +100,36 @@ def core(keywords):
                     print('Error occured while cleaning')
         return mega_json
 
-
-    cleaned_data = clean(result)
-
-    es = Elasticsearch()
-
     # added id generator method
     # uuid3 creates a unique id from a namespace and a string(the title, in our example)
     # these id's are reproducible, so if 2 id's are made with the same namespace 
     # and the same name(article_name) they should be equal
     # i messed with uuid in pycharm for a while and this method seemed to work
-
     def id_generator(article_name):
         unique_id = uuid.uuid3(uuid.NAMESPACE_DNS, article_name)
         return unique_id
 
 
-    cleaned_data = clean(result)
+    # Following code is to temporarily save the output
+    # cleaned_data = clean(result)
+    # f = open("cleandata.json", "w")
+    # f.write(json.dumps(cleaned_data))
+    # f.close()
 
-    es = Elasticsearch()
+    # f = open("rawdata.json", "w")
+    # f.write(json.dumps(result))
+    # f.close()
 
-    for doc in range(len(cleaned_data)):
-        #res = es.index(index="core", id=doc, body=cleaned_data[doc])
+    # es = Elasticsearch()
 
-        # changed index and id
-        # i assume we need to replace article_name, but not sure with what
-        res = es.index(index="amr", id=id_generator(cleaned_data[doc]['title']), body=cleaned_data[doc])
-        #if doc%100 == 0:
-        #    print(doc)
+    # for doc in range(len(cleaned_data)):
+    #     #res = es.index(index="core", id=doc, body=cleaned_data[doc])
 
-    es.indices.refresh(index="amr")
+    #     # changed index and id
+    #     # i assume we need to replace article_name, but not sure with what
+    #     res = es.index(index="amr", id=id_generator(cleaned_data[doc]['title']), body=cleaned_data[doc])
+    #     #if doc%100 == 0:
+    #     #    print(doc)
+
+    # es.indices.refresh(index="amr")
     print('Total documents in CORE: ' , len(cleaned_data))
--- python-scraper/indexer.py
+++ python-scraper/indexer.py
@@ -1,69 +1,68 @@
-import multiprocessing
+from threading import Thread
 import schedule
 import time
-from elasticsearch import Elasticsearch
-from database_Core import core
-from database_DOAJ import doaj
-from database_Unpaywall import unpaywall
+import os
+import ssl
 
-import os, ssl
+# from elasticsearch import Elasticsearch
+from database_Core import core
+# from database_DOAJ import doaj
+# from database_Unpaywall import unpaywall
+
+
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
     getattr(ssl, '_create_unverified_context', None)): 
     ssl._create_default_https_context = ssl._create_unverified_context
     
-'''def pp():
-    for i in range(10):
-        print(i)
-'''
 
-keywords = 'search.json'
+keywords = "python-scraper/search.json"
+# keywords = 'search.json'
 
 def runThreads(): 
-        pcore = multiprocessing.Process(target=core, args=(keywords,))
-        pdoaj = multiprocessing.Process(target=doaj, args=(keywords,))
-        punpaywall = multiprocessing.Process(target=unpaywall, args=(keywords,))
-        #p2 = multiprocessing.Process(target=pp)
+    start_time = time.time()
 
-        start_time = time.time()
-        pcore.start()
-        pdoaj.start()
-        punpaywall.start()
-        #p2.start()
+    # Define the threads that need to be run:
+    threadCallBacks = [core]
+    params = (keywords,)
+
+    # Create the thread objects 
+    threadObjects = []
+    for callback in threadCallBacks: 
+        print(params)
+        t = Thread(target=callback, args=params)
+        threadObjects.append(t)
+        t.start()
+    
+    # Join back with all threads
+    for thread in threadObjects:
         try: 
-            pcore.join()
-        except:
-            print("Error occured when indexing CORE")
-        try: 
-            pdoaj.join()
-        except:
-            print("Error occured when indexing DOAJ")
-        try: 
-            punpaywall.join()
-        except: 
-            print("Error occured when indexing Unpaywall")
-        #p2.join()
-        es = Elasticsearch()
-        es.indices.refresh("amr")
-        elasticSearchTotal = es.cat.count("amr")
-        print("Total documents indexed (epoch|timestamp|count): ", elasticSearchTotal)
-        e = int(time.time() - start_time) 
-        print('All processes done')  
-        print('Total indexing time: {:02d}:{:02d}:{:02d}'.format(e // 3600, (e % 3600 // 60), e % 60))
+            thread.join()
+        except Exception as error: 
+            print(error)
+  
+    # es = Elasticsearch()
+    # es.indices.refresh("amr")
+    # elasticSearchTotal = es.cat.count("amr")
+    # print("Total documents indexed (epoch|timestamp|count): ", elasticSearchTotal)
+    e = int(time.time() - start_time) 
+    print('All processes done')  
+    print('Total indexing time: {:02d}:{:02d}:{:02d}'.format(e // 3600, (e % 3600 // 60), e % 60))
 
 
 if __name__ == '__main__':
     print("Running processes")
     runThreads()
     print("Running the schedule")
-    try:
-        print(" Will rerun the scrapers at 11:59 PM")
-        schedule.every().day.at("23:59").do(runThreads) #Rerun the scrapers every 11:59 PM
-    except:
-        print("Error with scheduling")
 
-    while True: 
-        try:
-            schedule.run_pending()
-            time.sleep(1)
-        except:
-            print("Error with scheduling")
+    # try:
+    #     print(" Will rerun the scrapers at 11:59 PM")
+    #     schedule.every().day.at("23:59").do(runThreads) #Rerun the scrapers every 11:59 PM
+    # except:
+    #     print("Error with scheduling")
+
+    # while True: 
+    #     try:
+    #         schedule.run_pending()
+    #         time.sleep(1)
+    #     except:
+    #         print("Error with scheduling")
